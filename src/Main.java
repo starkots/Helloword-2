ad551b5c133fa2eac7308b330118593b67dbebca
starkots <starkots@mail.ru>
2023-02-20 00:05:10 +0300

    Первый коммит 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,100 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
+        System.out.println("Задача 1");
+
+
+            // используем тип переменной var
+            var dog = 8.0;
+            System.out.println(dog);
+            dog = dog + 4;
+
+
+            var cat = 3.6;
+            System.out.println(cat);
+            cat = cat + 4;
+
+            var papaer = 763789;
+            System.out.println(papaer);
+            papaer = papaer + 4;
+
+        System.out.println("Задача 2");
+
+
+            System.out.println(dog);
+
+            dog = dog + 3.5;
+            System.out.println(cat);
+
+            cat = cat + 1.6;
+            System.out.println(papaer);
+
+            papaer = papaer + 7639;
+
+        System.out.println("Задача 2");
+
+//используем переменную var
+            var friend = 19;
+            System.out.println(friend);
+            friend = friend + 2;
+            System.out.println(friend);
+            friend = friend / 7;
+            System.out.println(friend);
+
+        System.out.println("Задача 3");
+
+//используем переменную var
+            var frog = 3.5;
+            System.out.println(frog);
+            frog = frog * 10;
+            System.out.println(frog);
+            frog = frog / 3.5;
+            System.out.println(frog);
+            frog = frog + 4;
+
+        System.out.println("Задача 4");
+
+            // используем тип переменной var
+
+            var boks = 78.2;
+            var boks2 = 82.7;
+            var boksLeft = boks + boks2;
+            System.out.println(" Общий вес " + boksLeft + " кг ");
+
+            var boks3 = 78.2;
+            var boks4 = 82.7;
+            var boksWeight = boks3 / boks4;
+            System.out.println(" Общий вес " + boksWeight);
+
+        System.out.println("Задача 5");
+
+            var time = 640;
+            var time1 = 8;
+            var timeLeft = time / time1;
+            System.out.println(" Всего работников в компании " + timeLeft);
+
+        System.out.println("Задача 6");
+
+
+            var sotrudnik = 80;
+            var sotrudnik1 = 94;
+            var sotrudnikLeft = sotrudnik + sotrudnik1;
+            var sotrudnik2 = 174;
+            var sotrudnik3 = 8;
+            var sotrudnikWeight = sotrudnik2 / sotrudnik3;
+            System.out.println(" Если в компании работает " + sotrudnikLeft + " человека то всего " + sotrudnikWeight + " часов работы может быть поделено между сотрудниками ");
+
+            //Считаем перенменные var
+
+
+        System.out.println("Задача 7");
+
+
+
     }
-}
+
+
+    }
