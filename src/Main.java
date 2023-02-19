2c88c601220ddd2d1558fc4447cf9082e3c72fc3
starkots <starkots@mail.ru>
2023-02-20 00:53:32 +0300

    Первый коммит 1.2

--- src/Main.java
+++ src/Main.java
@@ -8,57 +8,44 @@ public class Main {
 
 
             // используем тип переменной var
-            var dog = 8.0;
-            dog = dog + 4;
-            System.out.println(dog);
-
-
-
-            var cat = 3.6;
-             cat = cat + 4;
+        var dog = 8.0;
+        System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        var cat = 3.6;
         System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+             var papaer = 763793;
+        System.out.println(papaer);
+        papaer = papaer + 7639;
+        System.out.println(papaer);
 
 
-
-            var papaer = 763789;
-             papaer = papaer + 4;
-            System.out.println(papaer);
-
-
-        System.out.println("Задача 2");
-
-
-            System.out.println(dog);
-            
-
-            dog = dog + 3.5;
-            System.out.println(cat);
-
-            cat = cat + 1.6;
-            System.out.println(papaer);
-
-            papaer = papaer + 7639;
 
         System.out.println("Задача 2");
 
 //используем переменную var
             var friend = 19;
-            System.out.println(friend);
-            friend = friend + 2;
-            System.out.println(friend);
-            friend = friend / 7;
+        friend = friend + 2;
+        friend = friend / 7;
+        System.out.println(friend);
+
+
+
+
 
 
         System.out.println("Задача 3");
 
 //используем переменную var
             var frog = 3.5;
-            System.out.println(frog);
-            frog = frog * 10;
-            System.out.println(frog);
-            frog = frog / 3.5;
-            System.out.println(frog);
-            frog = frog + 4;
+        frog = frog * 10;
+        frog = frog / 3.5;
+        frog = frog + 4;
+
+        System.out.println(frog);
+
 
         System.out.println("Задача 4");
 
