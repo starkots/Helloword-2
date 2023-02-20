3aa07a1a902229531d09f2f81aa44539513f489e
starkots <starkots@mail.ru>
2023-02-20 13:39:21 +0300

    Первый коммит 1.5

--- src/Main.java
+++ src/Main.java
@@ -89,6 +89,79 @@ public class Main {
         System.out.println("Задача 7");
 
         //
+        int a = 569;
+        byte b = 67;
+        short c = -159;
+        long e = 987678965549L;
+        float g = 27.12f;
+        double  f = 2.786;
+        char j = 27897;
+
+        System.out.println("Задача 8");
+
+        int schollLP = 23;
+        int schollAS = 27;
+        int scholl = 30;
+        int classL = 480;
+
+        int classL1 = schollLP + schollAS + scholl;
+        System.out.println(classL1);
+
+        int classL3 = classL / classL1;
+
+        System.out.println(classL3);
+
+        System.out.println("Задача 9");
+
+        int bat = 16;
+        int min = 2;
+        int etime = bat / min;
+        int etime2 = etime * 20;;
+        int etime4 = etime2 * 24;
+        int etime5 = etime4 * 3;
+        int etime6 = etime5 * 10;
+
+        System.out.println("За 20 минут машина произвела " + etime2 + " штук бутылок");
+        System.out.println("За сутки машина произвела " + etime4 + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + etime5 + " штук бутылок");
+        System.out.println("За месяц машина произвела " + etime6 + " штук бутылок");
+
+        System.out.println("Задача 5");
+
+        int BKol = 120;
+        int white = 2;
+        int kor = 4;
+        int ob = BKol / (white + kor);
+        int white1 = white * ob;
+        int kor1 = kor * ob;
+
+        System.out.println("В школе, где " + ob + " классов, нужно " + white1 + " банок белой краски и " + kor1 +  " банок коричневой краски");
+
+
+        System.out.println("Задача 6");
+
+        int bananas1  = 5;
+        int milk = 2;
+        int IceCream = 2;
+        int egg = 4;
+        int WeightBanana = 80;
+        int WeightMilk = 105;
+        int WeightIceCream = 100;
+        int WeightEgg = 70;
+
+        int totalWeightGr = bananas1 * WeightBanana + milk * WeightMilk + WeightIceCream * IceCream + egg * WeightEgg;
+        double totalWeightKG = totalWeightGr / 1_000;
+        System.out.println("Общий вес в граммах " + totalWeightGr + " в кг " + totalWeightKG);
+
+
+
+
+
+
+
+
+
+
 
 
 
